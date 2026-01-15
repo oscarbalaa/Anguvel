d6028099e910932e476e7ac3d203fda8798394f7
Leonardo Tavera <leonardo.tavera.anton@outlook.com>
2026-01-15 11:27:38 -0500

    w

--- anguvel/app/Repositories/ConsultaProyectoRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Repositories;
-
-interface ConsultaProyectoRepositoryInterface extends BaseRepositoryInterface
-{
-    // You can define custom methods for the ConsultaProyecto repository here
-}
--- anguvel/app/Repositories/ReporteIncidenciasRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Repositories;
-
-interface ReporteIncidenciasRepositoryInterface extends BaseRepositoryInterface
-{
-    // You can define custom methods for the ReporteIncidencias repository here
-}
