b13bda046a8f9404fd84a1abca29820741dbdcf9
Leonardo Tavera <leonardo.tavera.anton@outlook.com>
2026-01-20 14:21:49 -0500

    wuu

---
+++ anguvel-frontend/anguvel-frontend/src/app/app.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ApiService } from './api.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App implements OnInit {
+  protected readonly title = signal('anguvel-frontend');
+  
+  protected readonly data = signal<any | null>(null);
+  protected readonly error = signal<any | null>(null);
+
+  private readonly apiService = inject(ApiService);
+
+  ngOnInit(): void {
+    this.apiService.getDatos().subscribe({
+      next: (response) => {
+        this.data.set(response);
+        console.log('Datos recibidos:', response);
+      },
+      error: (err) => {
+        this.error.set(err);
+        console.error('Error al obtener los datos:', err);
+      }
+    });
+  }
+}
