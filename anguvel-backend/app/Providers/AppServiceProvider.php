64d9908d5135105a57f63fa4be3ab237e715f699
Leonardo Tavera <leonardo.tavera.anton@outlook.com>
2026-01-28 08:48:53 -0500

    wasa

--- anguvel-backend/app/Providers/AppServiceProvider.php
+++ anguvel-backend/app/Providers/AppServiceProvider.php
@@ -3,18 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Repositories\ConsultaProyectoRepositoryInterface;
-use App\Repositories\EloquentConsultaProyectoRepository;
-use App\Repositories\GestionTributariaPagosRepositoryInterface;
-use App\Repositories\EloquentGestionTributariaPagosRepository;
-use App\Repositories\ReporteIncidenciasRepositoryInterface;
-use App\Repositories\EloquentReporteIncidenciasRepository;
-use App\Repositories\SeguridadCiudadanaRepositoryInterface;
-use App\Repositories\EloquentSeguridadCiudadanaRepository;
-use App\Repositories\TramitesLicenciasRepositoryInterface;
-use App\Repositories\EloquentTramitesLicenciasRepository;
-use App\Repositories\UsuarioRepositoryInterface;
-use App\Repositories\EloquentUsuarioRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,12 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(ConsultaProyectoRepositoryInterface::class, EloquentConsultaProyectoRepository::class);
-        $this->app->bind(GestionTributariaPagosRepositoryInterface::class, EloquentGestionTributariaPagosRepository::class);
-        $this->app->bind(ReporteIncidenciasRepositoryInterface::class, EloquentReporteIncidenciasRepository::class);
-        $this->app->bind(SeguridadCiudadanaRepositoryInterface::class, EloquentSeguridadCiudadanaRepository::class);
-        $this->app->bind(TramitesLicenciasRepositoryInterface::class, EloquentTramitesLicenciasRepository::class);
-        $this->app->bind(UsuarioRepositoryInterface::class, EloquentUsuarioRepository::class);
+        //
     }
 
     /**
